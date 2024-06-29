a4d4ad262236f462cde0913dd80c3fc3d626fc16
ShivaayKansara <kansarashivaay@gmail.com>
2024-06-29 16:15:41 +0530

    Restaurant Web Page

---
+++ Restaurant_Web_Page/src/App.jsx
@@ -0,0 +1,10 @@
+
+import Restaurant from './components/Restaurant'
+
+const App = () => {
+  return (
+    <Restaurant/>
+  )
+}
+
+export default App
---
+++ Restaurant_Web_Page/src/components/Restaurant.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+import {useState} from "react";
+import Navbar from "./Navbar";
+import MenuCard from "./MenuCard"
+import Menu from "./MenuApi";
+import "./style.css";
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem)=>{
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+
+const Restaurant = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category)=>{
+    if(category === "All"){
+      setMenuData(Menu);
+      return;
+    }
+
+    const updateList = Menu.filter((curElem)=>{
+      return curElem.category === category;
+    });
+
+    setMenuData(updateList);
+  }
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList}/>
+      <MenuCard menuData={menuData} />
+    </>
+  )
+}
+
+export default Restaurant
